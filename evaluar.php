d807b67a2f66da86b09f43df82d747971fa088b7
Alberto Prado <albertoprado.desarrollador@gmail.com>
2019-03-01 12:20:21 -0600

    Cambios desconocidos

--- evaluar.php
+++ evaluar.php
@@ -6,30 +6,46 @@
     use Entrecore\GTMetrixClient\GTMetrixClient;
     use Entrecore\GTMetrixClient\GTMetrixTest;
 
-    // Generamos un nuevo cliente e iniciamos sesión 
-    $client = new GTMetrixClient();
-    $client->setUsername('albertoprado.desarrollador@gmail.com');
-    $client->setAPIKey('65358e7d9a9d5b044519477d29523938');
-    $client->getLocations();
-    $client->getBrowsers();
+    // // Generamos un nuevo cliente e iniciamos sesión 
+    // $client = new GTMetrixClient();
+    // $client->setUsername('albertoprado.desarrollador@gmail.com');
+    // $client->setAPIKey('65358e7d9a9d5b044519477d29523938');
+    // $client->getLocations();
+    // $client->getBrowsers();
 
-    // Iniciamos la prueba y esperamos recibir el resultado 
-    $test = $client->startTest($_POST["pagina"]);
-        while ($test->getState() != GTMetrixTest::STATE_COMPLETED &&
-        $test->getState() != GTMetrixTest::STATE_ERROR) {
-        $client->getTestStatus($test);
-        sleep(1);
-    }
+    // // Iniciamos la prueba y esperamos recibir el resultado 
+    // $test = $client->startTest($_POST["pagina"]);
+    //     while ($test->getState() != GTMetrixTest::STATE_COMPLETED &&
+    //     $test->getState() != GTMetrixTest::STATE_ERROR) {
+    //     $client->getTestStatus($test);
+    //     sleep(1);
+    // }
 
-    // Enviamos la información del reporte
-    $evaluacion = array("direccion" => $_POST["pagina"], 
-                         "id" => $test->getId(),
-                         "score" => $test->getPagespeedScore(), 
-                         "size" => $test->getHtmlBytes(), 
-                         "time" => $test->getHtmlLoadTime(),
-                         "ySlow" => $test->getYslowScore(),
-                         "report" => $test->getReportUrl()
-                        );
+    // // Enviamos la información del reporte
+    // $evaluacion = array("direccion" => $_POST["pagina"], 
+    //                      "id" => $test->getId(),
+    //                      "score" => $test->getPagespeedScore(), 
+    //                      "size" => $test->getHtmlBytes(), 
+    //                      "time" => $test->getHtmlLoadTime(),
+    //                      "ySlow" => $test->getYslowScore(),
+    //                      "report" => $test->getReportUrl(),
+    //                      "cargaHTML" => $test->getHtmlLoadTime(),
+    //                      "pageElements" => $test->getPageElements(),
+    //                      "fullyLoaded" => $test->getFullyLoadedTime()
+    //                     );
+    // echo json_encode($evaluacion);
+
+    $evaluacion = array("direccion" => "www.google.com.mx",
+                        "id" => "9d7axA",
+                        "score" => 93,
+                        "size" => 94847,
+                        "time" => 348,
+                        "yslow" => 83,
+                        "report" => '<a href="www.gtmetrix.com/api/0a39f.pdf"> GTMetrix </a>',
+                        "cargaHTML" => 84837, 
+                        "pageElements" => 489,
+                        "fullyLoaded" => 3821
+    );
     echo json_encode($evaluacion);
 
 ?>
--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // Enviamos los datos a la función para evaluar su desempeño
+var isBack = false;
+
 function evaluar() {
     
     // Desactivamos el uso de $ 
@@ -15,24 +17,34 @@ function evaluar() {
     }
 
     // Realizamos la petición ajax con los parametros de la evaluacion
-    jQuery.ajax({
-        data: parametros,
-        url: "evaluar.php",
-        type: "post", 
-        beforeSend: function() {
-            console.log("Evaluando pagina");
-            jQuery('#contenedor').append('<div class="alert alert-info mt-4 mb-2 id="notificacion"><strong> Generando Reporte! </strong> Espere un momento </div>');  
-        },
-        success: function(response) {
-            console.log("Evaluación completada: " + response); 
-            let respuesta = JSON.parse(response); 
-            jQuery('#contenedor').append('<div class="row p-3"><div id="detalles" class="col-sm-12 col-md-4 bg-light border p-1"> </div><div id="informacion" class="col-sm-12 col-md-8 border"> </div></div>');
-            jQuery('#detalles').append('<p class="text-center"><strong> Detalles de la Prueba </strong></p>');
-            jQuery('#detalles').append('<p> ID Prueba: ' + respuesta.id + '</p>');   
-            jQuery('#detalles').append('<p> Reporte completo de la prueba ')
-        }
-    });
-
+    if(!isBack) { 
+        isBack = true;
+        jQuery.ajax({
+            data: parametros,
+            url: "evaluar.php",
+            type: "post", 
+            beforeSend: function() {
+                console.log("Evaluando pagina");
+                jQuery('#contenedor').append('<div class="alert alert-info mt-4 mb-2 id="notificacion"><strong> Generando Reporte! </strong> Espere un momento </div>');  
+            },
+            success: function(response) {
+                console.log("Evaluación completada: " + response); 
+                let respuesta = JSON.parse(response); 
+                jQuery('#contenedor').append('<div class="row p-3"><div id="detalles" class="col-sm-12 col-md-4 p-2 detalles"> </div><div id="informacion" class="col-sm-12 col-md-8 border"> </div></div>');
+                jQuery('#detalles').append('<p class="text-center"><strong> Detalles de la Prueba </strong></p>');
+                jQuery('#detalles').append('<p> ID Prueba: ' + respuesta.id + '</p>');   
+                jQuery('#detalles').append('<p> Resultado Completo: ' +  respuesta.report);
+                jQuery('#informacion').append('<p><strong> Puntuación PageSpeed: ' +  respuesta.score + "% </strong></p>");
+                if(respuesta.score >= 80) {
+                    jQuery('#informacion').append('<p> Una puntuación mayor a 80 significa que su sitio requiere ajustes menores </p>');
+                }
+                jQuery('#informacion').append('<p><strong> Puntuación YSlow: ' +  respuesta.yslow + "% </strong></p>");
+                jQuery('#informacion').append('<p><strong> Tiempo de carga(ms): ' +  respuesta.time + "% </strong></p>");
+                jQuery('#informacion').append('<p><strong> Carga HTML: ' +  respuesta.cargaHTML + "% </strong></p>");
+                jQuery('#informacion').append('<p><strong> Page Elements: ' +  respuesta.pageElements + "% </strong></p>");
+            }
+        });
+    }
 
 
 }
